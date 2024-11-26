7d3209949f75595fab89049b18b38c7802d4c963
fiqAsh <ashfiq625@gmail.com>
2024-11-27 01:45:13 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<main>
+			<h1 className="text-5xl text-violet-300 font-bold ">hello welcome</h1>
+		</main>
+	);
+};
+
+export default App;
